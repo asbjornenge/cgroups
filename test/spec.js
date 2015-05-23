0f49598cc601a5db95288576028b77377ee2d39b
Asbjørn Enge <asbjorn@hanafjedle.net>
2015-05-23 09:57:00 +0200

    Made a wrapper

--- test/spec.js
+++ test/spec.js
@@ -3,14 +3,27 @@ var fs     = require('fs')
 var assert = require('assert')
 var cgroup = require('../index')
 
+var groupWrapper = function(opts, group, callback) {
+    cgroup.create(opts, group, function(err) {
+        var cleanUp = function(cb) {
+            cgroup.remove(opts, group, cb)
+        }
+        callback(err, cleanUp)
+    })
+}
+
 it('can create a cgroup', function(done) {
-    cgroup.create({resources : ['cpuset']}, 'testgroup', function(err) {
+    groupWrapper({resources : ['cpuset']}, 'testgroup', function(err, cleanup) {
         assert(!err)
         assert(fs.lstatSync(cgroup.root+'/cpuset/testgroup').isDirectory())
-        cgroup.remove({resources : ['cpuset']}, 'testgroup', function(err) {
+        cleanup(function(err) {
             assert(!err)
             assert(!fs.existsSync(cgroup.root+'/cpuset/testgroup'))
             done()
-        })
+        })        
     })
 })
+
+//it('can put a process inside a cgroup', function(done) {
+//    cgroup.create(
+//})
