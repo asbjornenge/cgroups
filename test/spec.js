f0fe5b49930c60dabd5005fe7e90e1cd73b8e60e
Asbjørn Enge <asbjorn@hanafjedle.net>
2015-05-24 22:36:38 +0200

    Cgroups and all tests

--- test/spec.js
+++ test/spec.js
@@ -1,36 +1,36 @@
 require('shelljs/global')
-var fs     = require('fs')
-var assert = require('assert')
-var chpr   = require('child_process')
-var cgroup = require('../index')
+var fs      = require('fs')
+var assert  = require('assert')
+var chpr    = require('child_process')
+var cgroups = require('../index')
 
 var withGroup = function(opts, group, callback) {
-    cgroup.create(opts, group, function(err) {
+    cgroups.create(opts, group, function(err) {
         var cleanUp = function(cb) {
-            cgroup.remove(opts, group, cb)
+            cgroups.remove(opts, group, cb)
         }
         callback(err, cleanUp)
     })
 }
 
-it('can create a cgroup', function(done) {
+it('can create a cgroups', function(done) {
     withGroup({resources : ['cpuset']}, 'testgroup', function(err, cleanup) {
         assert(!err)
-        assert(fs.lstatSync(cgroup.root+'/cpuset/testgroup').isDirectory())
+        assert(fs.lstatSync(cgroups.root+'/cpuset/testgroup').isDirectory())
         cleanup(function(err) {
             assert(!err)
-            assert(!fs.existsSync(cgroup.root+'/cpuset/testgroup'))
+            assert(!fs.existsSync(cgroups.root+'/cpuset/testgroup'))
             done()
         })        
     })
 })
 
-it.only('can set group values', function(done) {
+it('can set group values', function(done) {
     withGroup({resources : ['cpuset']}, 'testgroup2', function(err, cleanup) {
         assert(!err)
-        cgroup.set('testgroup2', { cpuset : { cpus : '0' }}, function(err) {
+        cgroups.set('testgroup2', { cpuset : { cpus : '0' }}, function(err) {
             assert(!err)
-            var numcpus = cat(cgroup.root+'/cpuset/testgroup2/cpuset.cpus').trim()
+            var numcpus = cat(cgroups.root+'/cpuset/testgroup2/cpuset.cpus').trim()
             assert(numcpus == '0')
             cleanup(done)
         })
@@ -38,11 +38,11 @@ it.only('can set group values', function(done) {
 })
 
 
-it('can put a process inside a cgroup', function(done) {
+it('can put a process inside a cgroups', function(done) {
     withGroup({resources : ['cpuset']}, 'testgroup3', function(err, cleanup) {
         assert(!err)
         var child = chpr.spawnSync('bash')
-        cgroup.getGroups(child.pid, function(err, groups) {
+        cgroups.getGroups(child.pid, function(err, groups) {
             cleanup(done)
         })
     })
