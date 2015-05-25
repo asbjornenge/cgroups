97e98ac811faf81f8142754243c28a83e18147de
Asbjørn Enge <asbjorn@hanafjedle.net>
2015-05-25 22:39:31 +0200

    Rewrote yolo

--- yolo.js
+++ yolo.js
@@ -2,48 +2,30 @@ var fs = require('fs')
 var chpr = require('child_process')
 var cgroups = require('./index')
 
-var handleErr = function(err) {
-    if(err) { console.error(err); process.exit(1) }
+var noErr = function(fn) {
+    return function(err) {
+        if(err) { console.error(err); process.exit(1) }
+        fn()
+    }
 }
 
+// We have a process
+var stress = chpr.spawn('bash')
+
 // Let's make a cgroup
-cgroups.create('yolo', { resources : ['cpuset'] }, function(err) {
-    handleErr(err)
-    // Limit the cgroup to 1 cpu only
-    cgroups.set('yolo', { cpuset : { cpus : '0' }}, function(err) {
-        handleErr(err)
-        // Make a bash and move to cgroup
-        var stress = chpr.spawn('bash')
-        stress.stdout.on('data', function(err) {
-            console.log('shell data',err)
-        })
-        stress.stderr.on('data', function(err) {
-            console.log('shell err',err)
-        })
-        console.log(stress.pid)
-        cgroups.movePid(stress.pid, 'cpuset/yolo', function(err) {
-            handleErr(err)
-            // Stress multiple cpus
-            stress.stdin.write('dd if=/dev/zero of=/dev/null | dd if=/dev/zero of=/dev/null | dd if=/dev/zero of=/dev/null\n')
-            stress.stdin.write('ps --ppid $$')
-        })
+cgroups.create('yolo', {cpuset : {cpus:'0'}}, noErr(function() {
+    // Put the process in the cgroup
+    cgroups.movePid(stress.pid, 'cpuset/yolo', noErr(function() {
+        stress.stdin.write("dd if=/dev/zero of=/dev/null | dd if=/dev/zero of=/dev/null | dd if=/dev/zero of=/dev/null | dd if=/dev/zero of=/dev/null \n")
+        console.log('Open htop, see 1 cpu spike! Current cmd should spike 4 cpus...')
+        console.log('*Controlled by cgroups - BOOMshakalaka*')
+    })) 
+}))
 
-
-        // Watch htop for a bit to verify that only 1 cpu is stressed
-        setTimeout(function() {
-            // Cleanup
-            console.log('Cleaning up...')
-            stress.stdin.write('pkill -P $$\n')
-//            setTimeout(function() {
-//                stress.on('exit', function() {
-//                    cgroups.remove('yolo', { resources : ['cpuset'] }, function(err) {
-//                        handleErr(err)
-//                        console.log('All done :-)')
-//                        process.exit(0)
-//                    })
-//                })
-//                stress.kill('SIGTERM')
-//            },1000)
-        }, 10000)
+// Cleanup on exit
+process.on('SIGINT', function() {
+    stress.kill('SIGINT')
+    cgroups.remove('yolo', {cpuset:{}}, function() {
+        process.emit('SINGINT')
     })
 })
